7493e1ed9a23ab09bd82c5e9bfc20c3433d545c7
Salim Bahassou <sbahasso@student.codam.nl>
2025-07-21 19:33:21 +0200

    Create ft_strlcpy.c
    first draft strlcpy.c

---
+++ ft_strlcpy.c
@@ -0,0 +1,18 @@
+size_t  ft_strlcpy(char *dest, const char *src, size_t size)
+{
+  size_t i;
+
+i = 0;
+if (size > 0)
+  {
+    while (src[i] && i < size - 1)
+    {
+      dest[i] = src[i];
+      i++;
+    }
+    dest[i] = '\0';
+  }
+  while (src[i])
+      i++;
+  return (i);
+}
