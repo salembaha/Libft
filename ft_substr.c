9ecc45510f9ec6b84fac2932941d675af856ddf8
Salim Bahassou <sbahasso@student.codam.nl>
2025-08-05 18:53:14 +0200

    Created ft_substr.c needs header

---
+++ ft_substr.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char			*substr;
+	size_t			str_len;
+	size_t			i;
+
+	if (!s)
+		return (NULL);
+	str_len = ft_strlen(s);
+	if (start >= str_len)
+		return (ft_strdup(""));
+	if (len > str_len - start)
+		len = str_len - start;
+	substr = (char *)malloc((len + 1) * sizeof(char));
+	if (!substr)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		substr[i] = s[start + i];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
