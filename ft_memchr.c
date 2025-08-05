ade469a6d2588ef03d15cf3acf272a59737470c6
Salim Bahassou <sbahasso@student.codam.nl>
2025-08-05 18:10:16 +0200

    Created ft_memchr.c needs header

---
+++ ft_memchr.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t				i;
+	const unsigned char	*str;
+
+	i = 0;
+	str = (const unsigned char *)s;
+	while (i < n)
+	{
+		if (str[i] == (unsigned char)c)
+			return ((void *)(str + i));
+		i++;
+	}
+	return (NULL);
+}
