d9c2c68fcaddaac222a8c02d897c79dc3ba5e949
Salim Bahassou <sbahasso@student.codam.nl>
2025-08-16 15:38:03 +0200

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -11,3 +11,29 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+    unsigned char *d;
+    const unsigned char *s;
+  
+    size_t i = 0;
+    if (dest == src || n == 0)
+        return dest;
+    d = (unsigned char *)dest;
+    s = (const unsigned char *)src;
+    if (d < s)
+    {
+        while (i < n)
+        {
+            d[i] = s[i];
+            i++;
+        }
+    }
+    else
+    {
+        while (n--)
+            d[n] = s[n];
+    }
+    return dest;
+}
