06a4663d353d2a2ac2e95f23b169dfc057f52609
Salim Bahassou <sbahasso@student.codam.nl>
2025-08-16 17:09:30 +0200

    Update ft_strlcpy.c "deleted main"

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -20,27 +20,3 @@ if (size > 0)
       i++;
   return (i);
 }
-
-int main(void) {
-    char dest[20];
-    const char *src = "Hello, world!";
-    size_t ret;
-
-    // Test 1: Normal copy
-    ret = ft_strlcpy(dest, src, sizeof(dest));
-    printf("Test 1: '%s' (returned %zu, expected %zu)\n", dest, ret, strlen(src));
-
-    // Test 2: Size == 0 (should not copy, but return length)
-    ret = ft_strlcpy(dest, src, 0);
-    printf("Test 2: '%s' (returned %zu, expected %zu)\n", dest, ret, strlen(src));
-
-    // Test 3: Size < src length (should truncate)
-    ret = ft_strlcpy(dest, src, 6);
-    printf("Test 3: '%s' (returned %zu, expected %zu)\n", dest, ret, strlen(src));
-
-    // Test 4: Empty src
-    ret = ft_strlcpy(dest, "", sizeof(dest));
-    printf("Test 4: '%s' (returned %zu, expected 0)\n", dest, ret);
-
-    return 0;
-}
