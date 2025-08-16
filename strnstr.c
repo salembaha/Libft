b50277e67ece98644329ee1debc1ff9ecfd24c31
Salim Bahassou <sbahasso@student.codam.nl>
2025-08-16 16:37:14 +0200

    Created strnstr.c needs header

---
+++ strnstr.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	if (*little == '\0')
+		return ((char *)big);
+	while (big[i] != '\0' && i < len)
+	{
+		j = 0;
+		while (little[j] != '\0' && (i + j) < len && big[i + j] == little[j])
+			j++;
+		if (little[j] == '\0')
+			return ((char *)&big[i]);
+		i++;
+	}
+	return (NULL);
+}
