f85b14158a7ed94c65c7fcfe1661c86d6bdf3691
Salim Bahassou <sbahasso@student.codam.nl>
2025-08-16 16:29:52 +0200

    Created ft_strlcat.c needs header

---
+++ ft_strlcat.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	src_len;
+	size_t	dest_len;
+
+	src_len = ft_strlen(src);
+	dest_len = ft_strlen(dest);
+	if (size <= dest_len)
+	{
+		return (size + src_len);
+	}
+	i = dest_len;
+	j = 0;
+	while (src[j] != '\0' && i < size - 1)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest_len + src_len);
+}
