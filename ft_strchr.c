bed02e868e1eb62c0428ff14ce52ea22b7e8aa06
Salim Bahassou <sbahasso@student.codam.nl>
2025-08-16 16:33:42 +0200

    Updated the logic of this function ft_strchr.c needs header

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,12 +1,12 @@
-char  *ft_strchr(const char *s, int c)
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
 {
-  while (*s)
-    {
-      is (*s == (char)c)
-        return ((char *)s);
-      s++;
-    }
-  if ((char)c == '0')
-    return ((char *)s);
-  return (NULL);
+	while ((char)c != *s)
+	{
+		if (!*s)
+			return (0);
+		s++;
+	}
+	return ((char *)s);
 }
